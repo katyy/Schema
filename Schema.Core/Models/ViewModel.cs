01f260e4dbfe707f25ec09034bc00f7066dbc252
katyy <sirychenka.katy@gmail.com>
2012-03-11 17:56:02 +0300

    add Models/ViewModel.cs

---
+++ Schema.Core/Models/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Schema.Core.Models
+{
+   public class ViewModel
+    {
+       public string Name { get; set; }
+       public string TypeDescription { get; set; }
+     //  public List<Column> Columns { get; set; }
+       //public List<TrigerModel> Trigers { get; set; }
+       //public List<IndexModel> Indexes { get; set; }
+    }
+}
