71f45c70b2af2a0de9e56e19cc74019544e44786
katyy <sirychenka.katy@gmail.com>
2012-04-11 09:57:58 +0300

    TableNames added

---
+++ Schema.Core/Names/TableNames.cs
@@ -0,0 +1,16 @@
+﻿namespace Schema.Core.Keys
+{
+    public class TableNames
+    {
+        public const string Tables = "Tables";
+        public const string Keys = "Keys";
+        public const string ForigenKey = "ForigenKey";
+        public const string Triggers = "Triggers";
+        public const string Indexes = "Indexes";
+        public const string Views = "Views";
+        public const string Procedures = "Procedures";
+        public const string ViewTriggers = "ViewTriggers";
+        public const string ViewIndexes = "ViewIndexes";
+        public const string Functions = "Functions";
+    }
+}
